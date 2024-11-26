e4039d74c5ab82fd82235461f7bd0561e52e6400
totsapol <totsapol.piluk@gmail.com>
2024-11-26 14:57:54 +0700

    Add () : Create new project

---
+++ Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,5 @@
+﻿namespace Avalonia.MusicStore.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase {
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Avalonia.MusicStore/Views/MainWindow.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace Avalonia.MusicStore.Views;
+
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+    }
+}
